efb36557488eb7136c990c5bf4e5487b0defb045
Jason Suarez <swarecito@gmail.com>
2023-10-20 19:21:10 +0200

    Add env variables

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
+import { env } from "~/env.mjs";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAgYHwP_IAoQDcOllaEum1GTck-QJypdUk",
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "chut-next.firebaseapp.com",
   projectId: "chut-next",
   storageBucket: "chut-next.appspot.com",
