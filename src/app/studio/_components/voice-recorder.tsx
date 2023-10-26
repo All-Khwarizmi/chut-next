85aff788c4f6ed4ad6ad6922da703a8884668f30
Jason Suarez <swarecito@gmail.com>
2023-10-26 13:56:52 +0200

    Update voice recorder

--- src/app/studio/_components/voice-recorder.tsx
+++ src/app/studio/_components/voice-recorder.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { AudioRecorder } from "react-audio-voice-recorder";
+import React, { useState, useEffect, useRef } from "react";
 import { initFirebase, storageBucket } from "~/utils/firebase";
 import { getAuth } from "@firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -11,15 +10,21 @@ import {
 } from "firebase/storage";
 import { TextField } from "@mui/material";
 import { useStore } from "~/utils/stores/stores";
+import { useAudioRecorder, AudioRecorder } from "react-audio-voice-recorder";
+import { HiPlayPause } from "react-icons/hi2";
+import { MdStopCircle } from "react-icons/md";
+import { AudioVisualizer, LiveAudioVisualizer } from "react-audio-visualize";
 
 interface VoiceRecorderProps {
   // onSave: (audioBlob: Blob) => void;
 }
 
 const VoiceRecorder: React.FC<VoiceRecorderProps> = ({}) => {
-  const [inputField, setInputField] = useState<string | null>(null);
-  const [isRecording, setIsRecording] = useState(false);
+  const [inputField, setInputField] = useState<string>("");
+  const [isRecordingOld, setIsRecording] = useState(false);
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [mediaRecorderLocal, setMediaRecorderLocal] =
+    useState<MediaRecorder | null>(null);
   const app = initFirebase();
   const auth = getAuth(app);
   const [user] = useAuthState(auth);
@@ -29,8 +34,31 @@ const VoiceRecorder: React.FC<VoiceRecorderProps> = ({}) => {
     state.setUserRecords,
   ]);
 
+  // new
+  const visualizerRef = useRef<HTMLCanvasElement>(null);
+  const {
+    startRecording,
+    stopRecording,
+    togglePauseResume,
+    recordingBlob,
+    isRecording,
+    isPaused,
+    recordingTime,
+    mediaRecorder,
+  } = useAudioRecorder();
+  const recorderControls = useAudioRecorder();
+  useEffect(() => {
+    console.log("Is recording: ", isRecording);
+    console.log("Is paused: ", isPaused);
+    console.log("recoding time: ", recordingTime);
+    console.log("Media recorder: ", mediaRecorder);
+    if (!mediaRecorderLocal && mediaRecorder) {
+      setMediaRecorderLocal(mediaRecorder);
+    }
+  }, [isRecording, isPaused, recordingTime]);
+
   const handleUploadFile = (blob: Blob) => {
-    if (blob && inputField && inputField.length !== 0) {
+    if (blob && inputField.length !== 0) {
       const storageRef = ref(
         storageBucket,
         `customers/${user?.uid}/records/${inputField}`,
@@ -67,29 +95,29 @@ const VoiceRecorder: React.FC<VoiceRecorderProps> = ({}) => {
             setUserRecords({ label: inputField, value: downloadURL });
             setUpdate(!update);
             setUploadProgress(0); // Reset the progress
-            setInputField(null);
+            setInputField("");
           });
         },
       );
     }
   };
 
-  return (
+  const oldRecorder = (
     <div
       id="user-records"
       className="place flex w-[360px] flex-col place-items-center gap-y-3 rounded-lg bg-mediumGray p-4"
     >
       <TextField
         InputLabelProps={{ style: { color: "white" } }}
-        inputProps={{ style: { color: "white" } }}
-        sx={{ pt: 2, pb: 2, input: { color: "white" } }}
+        inputProps={{ width: 300, style: { color: "white" } }}
+        sx={{ width: 300, pt: 2, pb: 2, input: { color: "white" } }}
         required
         id="filled-required"
         label="Name"
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
           setInputField(event.target.value);
         }}
-        defaultValue="Silence"
+        value={inputField}
         variant="filled"
       />
       <AudioRecorder
@@ -101,7 +129,7 @@ const VoiceRecorder: React.FC<VoiceRecorderProps> = ({}) => {
         downloadOnSavePress={false}
         downloadFileExtension="webm"
       />
-      {isRecording && (
+      {isRecordingOld && (
         <div className="mt-2">
           <div className="relative h-2 w-full bg-blue-200">
             <div
@@ -116,6 +144,127 @@ const VoiceRecorder: React.FC<VoiceRecorderProps> = ({}) => {
       )}
     </div>
   );
+
+  const textInput = (
+    <TextField
+      InputLabelProps={{ style: { color: "white" } }}
+      inputProps={{ style: { color: "white" } }}
+      sx={{ pt: 2, pb: 2, input: { color: "white" } }}
+      required
+      id="filled-required"
+      label="Name"
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputField(event.target.value);
+      }}
+      value={inputField}
+      variant="filled"
+    />
+  );
+  const uploadProgression = isRecordingOld && (
+    <div className="mt-2">
+      <div className="relative h-2 w-full bg-blue-200">
+        <div
+          className="h-2 bg-blue-500"
+          style={{ width: `${uploadProgress}%` }}
+        ></div>
+      </div>
+      <p className="mt-2 text-center text-sm">
+        Upload Progress: {uploadProgress}%
+      </p>
+    </div>
+  );
+  const recorderBloc = (
+    <AudioRecorder
+      onRecordingComplete={handleUploadFile}
+      audioTrackConstraints={{
+        noiseSuppression: true,
+        echoCancellation: true,
+      }}
+      downloadOnSavePress={false}
+      downloadFileExtension="webm"
+      recorderControls={recorderControls}
+      showVisualizer={true}
+    />
+  );
+  const recordControls = (
+    <div>
+      <div className="flex flex-row gap-x-2 py-4">
+        <button
+          onClick={() => {
+            if (!isRecording) {
+              startRecording();
+            } else {
+              togglePauseResume();
+            }
+          }}
+          className="flex grow place-content-center rounded-lg bg-green-400 p-4 text-3xl"
+        >
+          <HiPlayPause />
+        </button>
+
+        <button
+          onClick={() => {
+            stopRecording();
+          }}
+          className="flex grow place-content-center rounded-lg bg-red-500 p-4 text-3xl"
+        >
+          <MdStopCircle />
+        </button>
+      </div>
+      <div className="">
+        <button
+          onClick={() => {
+            console.log("Saving file", recordingBlob);
+
+            if (isRecording) {
+              stopRecording();
+            }
+            if (recordingBlob) {
+              handleUploadFile(recordingBlob);
+            } else {
+              alert("Vous devez enregistrer un audio avant de le télécharger");
+            }
+          }}
+          className="w-[230px] rounded-lg bg-blue-500 p-4"
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  );
+  const audioVisualizer = mediaRecorderLocal && (
+    <div className="flex flex-row items-center gap-x-2">
+      <div className="text-lg  text-slate-200">
+        {durationFormatter(recordingTime)}
+      </div>
+      <LiveAudioVisualizer
+        mediaRecorder={mediaRecorderLocal}
+        width={200}
+        height={30}
+      />
+    </div>
+  );
+  const newRecorder = (
+    <div
+      id="user-records"
+      className="place flex w-[360px] flex-col place-items-center gap-y-3 rounded-lg bg-mediumGray p-4"
+    >
+      {textInput}
+      {audioVisualizer}
+      {recordControls}
+      {uploadProgression}
+    </div>
+  );
+
+  return newRecorder;
 };
 
 export default VoiceRecorder;
+
+export const durationFormatter = (duration: number) => {
+  const minutes = Math.floor(duration / 60);
+  const seconds = duration % 60;
+  return `${minutes.toFixed(0).padStart(2, "0")}:${seconds
+    .toFixed(0)
+    .padStart(2, "0")}`;
+};
