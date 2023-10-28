64bb3ea2d1a1546ce9e7c93dd0ee29ebad39df3f
Jason Suarez <swarecito@gmail.com>
2023-10-28 19:06:08 +0200

    change audio source

--- src/app/_components/meter/display-emoji-and-sound.tsx
+++ src/app/_components/meter/display-emoji-and-sound.tsx
@@ -8,33 +8,42 @@ export interface DisplayEmojiProps {
   isSound: boolean;
 }
 export function DisplayEmoji({ threshold, sound, isSound }: DisplayEmojiProps) {
-  const [sessionArr, isSoundPlaying, setIsSoundPlaying, setRecording] =
-    useStore((state) => [
-      state.sessionArr,
-      state.isSoundPlaying,
-      state.setIsSoundPlaying,
-      state.setRecording,
-    ]);
+  const [
+    sessionArr,
+    isSoundPlaying,
+    setIsSoundPlaying,
+    setRecording,
+    soundRef,
+  ] = useStore((state) => [
+    state.sessionArr,
+    state.isSoundPlaying,
+    state.setIsSoundPlaying,
+    state.setRecording,
+    state.soundRef,
+  ]);
 
   const emoji = getEmoji(sessionArr, threshold);
 
-  if (isTooLoud(sessionArr, threshold) && isSound && !isSoundPlaying) {
-    setIsSoundPlaying(true);
-    sound
-      .play()
-      .then(() => {
-        setTimeout(
-          () => setIsSoundPlaying(false),
-          sound.duration * 1000 + 2000,
-        );
-      })
-      .catch((e) => {
-        setRecording(false);
-        setIsSoundPlaying(false);
-        alert(`Something wrong happened trying to display the audio: ${e}`);
-      });
-  }
-  // useEffect(() => {}, [isSoundPlaying, isSound, sessionArr.length]);
+  useEffect(() => {
+    if (isTooLoud(sessionArr, threshold) && isSound && !isSoundPlaying) {
+      setIsSoundPlaying(true);
+      const audio = new Audio();
+      audio.src = soundRef;
+      audio
+        .play()
+        .then(() => {
+          setTimeout(
+            () => setIsSoundPlaying(false),
+            sound.duration * 1000 + 2000,
+          );
+        })
+        .catch((e) => {
+          setRecording(false);
+          setIsSoundPlaying(false);
+          alert(`Something wrong happened trying to display the audio: ${e}`);
+        });
+    }
+  }, [isSoundPlaying, isSound, sessionArr.length]);
 
   return <div className="pb-3 text-9xl">{emoji}</div>;
 }
