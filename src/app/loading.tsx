7d83fd8b4adcf4e10a1771076b86cc2951cfbd19
Jason Suarez <swarecito@gmail.com>
2023-11-10 20:45:49 +0100

    Add @next/bundle-analyzer package and update loading page layout

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,15 +2,17 @@ import { Stack, Skeleton } from "@mui/material";
 
 const LoadingPage = () => {
   return (
-    <Stack spacing={1}>
-      {/* For variant="text", adjust the height via font-size */}
-      <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
+    <div className="flex place-content-center">
+      <Stack spacing={1}>
+        {/* For variant="text", adjust the height via font-size */}
+        <Skeleton variant="text" sx={{ fontSize: "1rem" }} />
 
-      {/* For other variants, adjust the size with `width` and `height` */}
-      <Skeleton variant="circular" width={40} height={40} />
-      <Skeleton variant="rectangular" width={210} height={60} />
-      <Skeleton variant="rounded" width={210} height={60} />
-    </Stack>
+        {/* For other variants, adjust the size with `width` and `height` */}
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="rectangular" width={210} height={60} />
+        <Skeleton variant="rounded" width={210} height={60} />
+      </Stack>
+    </div>
   );
 };
 
